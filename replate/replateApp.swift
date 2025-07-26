7fbe533a4cd89fb30b1acdd64fb00fcf382181d4
YGao2005 <41841169+YGao2005@users.noreply.github.com>
2025-07-26 09:55:23 -0700

    Initial Commit

---
+++ replate/replateApp.swift
@@ -0,0 +1,17 @@
+//
+//  replateApp.swift
+//  replate
+//
+//  Created by Yang Gao on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct replateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ replateTests/replateTests.swift
@@ -0,0 +1,17 @@
+//
+//  replateTests.swift
+//  replateTests
+//
+//  Created by Yang Gao on 7/26/25.
+//
+
+import Testing
+@testable import replate
+
+struct replateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
